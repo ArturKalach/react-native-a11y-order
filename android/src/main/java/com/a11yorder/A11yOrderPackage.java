70b485604f8e639c6cbe072044da2e57f503fc96
Artur Kalach <artur.kalach.andreevich@gmail.com>
2025-02-02 15:56:06 +0100

    feat: update package

--- android/src/main/java/com/a11yorder/A11yOrderPackage.java
+++ android/src/main/java/com/a11yorder/A11yOrderPackage.java
@@ -1,53 +1,30 @@
 package com.a11yorder;
 
-
 import androidx.annotation.Nullable;
 
 import com.a11yorder.views.A11yGroupView.A11yGroupViewManager;
 import com.a11yorder.views.A11yIndexView.A11yIndexViewManager;
 import com.a11yorder.views.A11yOrderView.A11yOrderViewManager;
+import com.facebook.react.BaseReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.module.model.ReactModuleInfo;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
-import com.facebook.react.TurboReactPackage;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class A11yOrderPackage extends TurboReactPackage {
+public class A11yOrderPackage extends BaseReactPackage {
 
   @Nullable
   @Override
   public NativeModule getModule(String name, ReactApplicationContext reactContext) {
-    if (name.equals(A11yOrderModule.NAME)) {
-      return new A11yOrderModule(reactContext);
-    } else {
       return null;
-    }
   }
 
   @Override
   public ReactModuleInfoProvider getReactModuleInfoProvider() {
-    return () -> {
-      final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
-      boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-      moduleInfos.put(
-        A11yOrderModule.NAME,
-        new ReactModuleInfo(
-          A11yOrderModule.NAME,
-          A11yOrderModule.NAME,
-          false, // canOverrideExistingModule
-          false, // needsEagerInit
-          true, // hasConstants
-          false, // isCxxModule
-          isTurboModule // isTurboModule
-        ));
-      return moduleInfos;
-    };
+    return null;
   }
 
   @Override
