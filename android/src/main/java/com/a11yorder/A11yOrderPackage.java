93f27c9453b4cba32a32f03614996f49323f15ab
Artur Kalach <artur.kalach.andreevich@gmail.com>
2025-02-02 18:41:13 +0100

    fix: package fix

--- android/src/main/java/com/a11yorder/A11yOrderPackage.java
+++ android/src/main/java/com/a11yorder/A11yOrderPackage.java
@@ -5,34 +5,33 @@ import androidx.annotation.Nullable;
 import com.a11yorder.views.A11yGroupView.A11yGroupViewManager;
 import com.a11yorder.views.A11yIndexView.A11yIndexViewManager;
 import com.a11yorder.views.A11yOrderView.A11yOrderViewManager;
-import com.facebook.react.BaseReactPackage;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class A11yOrderPackage extends BaseReactPackage {
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
 
-  @Nullable
-  @Override
-  public NativeModule getModule(String name, ReactApplicationContext reactContext) {
-      return null;
-  }
+import java.util.ArrayList;
+import java.util.List;
 
-  @Override
-  public ReactModuleInfoProvider getReactModuleInfoProvider() {
-    return null;
-  }
+public class A11yOrderPackage implements ReactPackage {
 
-  @Override
-  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-    List<ViewManager> viewManagers = new ArrayList<>();
-    viewManagers.add(new A11yIndexViewManager());
-    viewManagers.add(new A11yOrderViewManager());
-    viewManagers.add(new A11yGroupViewManager());
-    return viewManagers;
-  }
+     @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        List<ViewManager> viewManagers = new ArrayList<>();
+        viewManagers.add(new A11yIndexViewManager());
+        viewManagers.add(new A11yOrderViewManager());
+        viewManagers.add(new A11yGroupViewManager());
+
+        return viewManagers;
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return new ArrayList<>();
+    }
 }
