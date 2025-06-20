fde0ef2b903065c9805d1f9870888c765b659ccb
Artur Kalach <artur_kalach@epam.com>
2025-06-20 22:51:46 +0200

    feat: add group order example

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { A11yIndex } from './components/A11yIndex/A11yIndex';
 import { A11yIndexSequence } from './components/A11ySequence/A11ySequence';
 import { A11yGroup } from './components/A11yGroup/A11yGroup';
 
-export { A11yOrder } from './components/A11yOrder/A11yOrder';
-export type { A11yOrderProps } from './types/A11yOrder.types';
 export type { IndexCommands } from './types/A11yIndex.types';
 
 export const A11y = {
