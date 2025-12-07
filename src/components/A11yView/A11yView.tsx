a0207aefbdb73a2a6e081b53b90cd99e106c9645
Artur Kalach <Artur Kalach>
2025-12-07 19:17:17 +0100

    fix: add ref A11yView

--- src/components/A11yView/A11yView.tsx
+++ src/components/A11yView/A11yView.tsx
@@ -1,54 +1,61 @@
 import React from 'react';
 import A11yViewNativeComponent from '../../nativeSpecs/A11yViewNativeComponent';
 import type { A11yViewProps } from '../../types/A11yView.types';
+import { View } from 'react-native';
 
-export const A11yView = React.memo<A11yViewProps>(
-  ({
-    onScreenReaderFocused,
-    onScreenReaderSubViewFocusChange,
-    onScreenReaderSubViewFocused,
-    onScreenReaderSubViewBlurred,
-    onScreenReaderDescendantFocusChanged,
-    ...props
-  }) => {
-    const hasHandler = Boolean(
-      onScreenReaderSubViewBlurred ||
-        onScreenReaderSubViewFocused ||
-        onScreenReaderSubViewFocusChange
-    );
-
-    const onScreenReaderChangeHandler = React.useCallback(
-      (event: { nativeEvent: { isFocused: boolean } }) => {
-        onScreenReaderSubViewFocusChange?.(event.nativeEvent.isFocused);
-        if (event.nativeEvent.isFocused) {
-          onScreenReaderSubViewFocused?.();
-        } else {
-          onScreenReaderSubViewBlurred?.();
-        }
-      },
-      [
+export const A11yView = React.memo(
+  React.forwardRef<View, A11yViewProps>(
+    (
+      {
+        onScreenReaderFocused,
         onScreenReaderSubViewFocusChange,
-        onScreenReaderSubViewBlurred,
         onScreenReaderSubViewFocused,
-      ]
-    );
+        onScreenReaderSubViewBlurred,
+        onScreenReaderDescendantFocusChanged,
+        ...props
+      },
+      ref
+    ) => {
+      const hasHandler = Boolean(
+        onScreenReaderSubViewBlurred ||
+          onScreenReaderSubViewFocused ||
+          onScreenReaderSubViewFocusChange
+      );
 
-    const onScreenReaderHandlerProp = hasHandler
-      ? onScreenReaderChangeHandler
-      : undefined;
+      const onScreenReaderChangeHandler = React.useCallback(
+        (event: { nativeEvent: { isFocused: boolean } }) => {
+          onScreenReaderSubViewFocusChange?.(event.nativeEvent.isFocused);
+          if (event.nativeEvent.isFocused) {
+            onScreenReaderSubViewFocused?.();
+          } else {
+            onScreenReaderSubViewBlurred?.();
+          }
+        },
+        [
+          onScreenReaderSubViewFocusChange,
+          onScreenReaderSubViewBlurred,
+          onScreenReaderSubViewFocused,
+        ]
+      );
 
-    return (
-      <A11yViewNativeComponent
-        {...props}
-        descendantFocusChangedEnabled={Boolean(
-          onScreenReaderDescendantFocusChanged
-        )}
-        onScreenReaderFocused={onScreenReaderFocused}
-        onScreenReaderFocusChange={onScreenReaderHandlerProp}
-        onScreenReaderDescendantFocusChanged={
-          onScreenReaderDescendantFocusChanged
-        }
-      />
-    );
-  }
+      const onScreenReaderHandlerProp = hasHandler
+        ? onScreenReaderChangeHandler
+        : undefined;
+
+      return (
+        <A11yViewNativeComponent
+          {...props}
+          ref={ref}
+          descendantFocusChangedEnabled={Boolean(
+            onScreenReaderDescendantFocusChanged
+          )}
+          onScreenReaderFocused={onScreenReaderFocused}
+          onScreenReaderFocusChange={onScreenReaderHandlerProp}
+          onScreenReaderDescendantFocusChanged={
+            onScreenReaderDescendantFocusChanged
+          }
+        />
+      );
+    }
+  )
 );
