7247ccd3a57dd164a4388e5ed11ffdfc8ddc984e
Artur Kalach <artur.kalach.andreevich@gmail.com>
2026-05-09 19:28:07 +0200

    fix: fix old arch code

---
+++ android/src/oldarch/A11yCardViewManagerSpec.java
@@ -0,0 +1,8 @@
+package com.a11yorder;
+
+import android.view.ViewGroup;
+
+import com.facebook.react.views.view.ReactViewManager;
+
+public abstract class A11yCardViewManagerSpec<T extends ViewGroup> extends ReactViewManager {
+}
